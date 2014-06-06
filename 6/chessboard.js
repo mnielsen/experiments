c6a18ecbd3fd3100be7c8e8b2cb1cd6e881dcc3f
Michael Nielsen <mn@michaelnielsen.org>
2014-06-06 14:31:59 -0400

    Minor improvements, a bugfix or two, few changes in functionality

--- 6/chessboard.js
+++ 6/chessboard.js
@@ -2,7 +2,7 @@
 
 $(function() {
     var canvas = $("#board")[0]
-    var board = canvas.getContext("2d");
+    var ctx = canvas.getContext("2d");
     var size = 32; 
     var xmin = 0;
     var ymin = 0;
@@ -10,14 +10,18 @@ $(function() {
     var data = {};
     for (var x = 0; x < 8; x++) {
 	for (var y = 0; y < 8; y++) {
-	    data[[x,y]] = (x+y) % 2;
+	    data[label(x,y)] = (x+y) % 2;
 	    drawSquare(x, y, data);
 	}
     }
 	
+    function label(x, y) {
+	return (x+y*8).toString()
+    }
+
     function drawSquare(x, y, data) {
-	board.fillStyle = (data[[x, y]] == 0 ? "white" : "black");
-	board.fillRect(xmin+x*size, ymin+y*size, size, size);
+	ctx.fillStyle = (data[label(x, y)] == 0 ? "white" : "black");
+	ctx.fillRect(xmin+x*size, ymin+y*size, size, size);
     }
 
     canvas.addEventListener("click", function(e) {
@@ -26,9 +30,9 @@ $(function() {
 	var mouseY = mousePos.y;
 	var x = Math.floor(mouseX/size);
 	var y = Math.floor(mouseY/size);
-	data[[x, y]] = 1-data[[x, y]];
+	data[label(x,y)] = 1-data[label(x, y)];
 	drawSquare(x, y, data);
-    }, false);
+    });
 
     function getMousePos(canvas, e) {
 	var rect = canvas.getBoundingClientRect();
